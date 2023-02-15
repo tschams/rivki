37f631098cff1f692a8c26b3026808b9237fe5dc
yiscaGabay <yigabay@g.jct.ac.il>
2023-02-15 11:46:46 +0200

    15/02

--- client.js
+++ client.js
@@ -1,6 +1,72 @@
+addEventListener("load", init)
+addEventListener("load", showContacts)
+
+function init() {
+    let AmountOfContacts = 0;
+    AmountOfContacts = localStorage.AmountOfContacts;
+    
+    if (AmountOfContacts) {
+        for (let i = 1; i <= AmountOfContacts; i++) {
+            let contact = JSON.parse(localStorage.getItem(`contact#${i}`));
+        }
+    }
+    else{
+        contact = {};
+        contact.name = 'aaa';
+        contact.phone = '0501111111';
+        contact.email = 'aaa@gmail.com'
+        contact = JSON.stringify(contact);
+        AmountOfContacts += 1;
+        localStorage.setItem(`contact#${AmountOfUsers}`, contact);
+        contact = {};
+        contact.name = 'bbb';
+        contact.phone = '0502222222';
+        contact.email = 'bbb@gmail.com'
+        contact = JSON.stringify(contact);
+        AmountOfContacts += 1;
+        localStorage.AmountOfContacts = AmountOfContacts;
+    }
+}
+
+function showContacts(){
+    // Create an XMLHttpRequest object
+    const xhttp = new XMLHttpRequest();
+    alert('inbbbb')
+    // Define a callback function
+    xhttp.onload = function() {
+        alert('111');
+        resp = this.responseText;
+        alert(resp);
+        AmountOfContacts = resp.AmountOfContacts;
+        alert(AmountOfContacts);
+        if (AmountOfContacts) {
+            for (let i = 1; i <= AmountOfContacts; i++) {
+                resp = JSON.parse(resp);
+                let contact = resp(`contact$(i)`);
+                document.getElementById("myUL").innerHTML += <li><a href="#">contact.name</a></li>;
+            }
+        }
+        let contact= {};
+        for (let i = 1; i <= 5; i++) {     
+            contact.name = 'contact ' + i;
+            //document.getElementById("myUL").innerHTML += <li><a href="#">contact.name</a></li>;
+            var node = document.createElement('li');
+            alert('in for 2')
+            node.appendChild(document.createTextNode('1111111'));
+            node.href = '#';
+            document.querySelector('ul').appendChild(node);
+            document.getElementById('myUL').innerHTML = node;
+        }
+    }
+
+    // Send a request
+    xhttp.open("GET", "ajax_info.txt");
+    xhttp.send();
+}
+
 function search() {
     var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
+    input = document.getElementById("mySearch");
     filter = input.value.toUpperCase();
     ul = document.getElementById("myUL");
     li = ul.getElementsByTagName("li");
@@ -13,4 +79,11 @@ function search() {
             li[i].style.display = "none";
         }
     }
+}
+
+function showOneItem(){
+    let contact = resp(`contact$(i)`);
+    document.getElementById('show_name').innerHTML = contact.name;
+    document.getElementById('show_phone').innerHTML = contact.phone;
+    document.getElementById('show_email').innerHTML = contact.email;
 }
