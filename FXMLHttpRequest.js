5970fd390ab4c47211bab01b37d56288784f6230
shiffy01 <sscheine@g.jct.ac.il>
2023-02-22 22:55:25 +0200

    some comments

---
+++ FXMLHttpRequest.js
@@ -0,0 +1,45 @@
+class FXMLHttpRequest {
+  constructor(){
+    this.onload()=null;//function to activate when data is loaded... check how to define template function
+    this.responsetext=null;//this will contain the data we return (in string format--needs to be parsed)
+   //responseXML  can return data as xml
+  }
+  open(method, file){//method=GET, POST, PUT, DELETE. file in our case is always database... see if this is needed
+    //put is update post is set new item
+    let fajax="";
+    //could be the following code is in the send function?:
+    //creates http request in the right format
+    let ans=request(fajax);//check how we have access to the network file
+    //disect ans and set responsetext
+
+    
+  }
+  send(){//for get requests according to w3school but why wouldnt you need info to say what you are getting?
+    if(this.onload!=null){//prob should just define it as empty function in the constructor
+      this.onload();//activates onload function
+    }
+  }
+  send(info){//is id contact, id, key and value...?
+
+  }
+  //research the following funcs:
+  //setRequestHeader()	Adds a label/value pair to the header to be sent
+  //xhttp.open("GET", "demo_get2.asp?fname=Henry&lname=Ford");
+
+  }
+  /*
+  status + statustext:
+  200 ok
+  403 forbidden
+  404 not found
+  */
+
+  /* this.readystate=0;
+    
+    0: request not initialized
+    1: server connection established
+    2: request received
+    3: processing request
+    4: request finished and response ready
+    //not sure if we need this seems kind of stupid...
+    */ 
