e60a7102a23397dd0344cad802d5ad706db13140
shiffy01 <sscheine@g.jct.ac.il>
2023-03-02 14:39:18 +0200

    more server

--- server.js
+++ server.js
@@ -13,18 +13,90 @@ function serverrecieve(fajax)
     let statusnum=404;
     let statustext="not found";
     if(method==="POST"){//add contact
-      if(typeof fajax.data === contact){
+      if(fajax.data instanceof contact){
         database.set(fajax.username, fajax.data);
       statusnum=200;
       statustext="ok";
       }
-      if(typeof fajax.data === user){//user sign up
-        let check= database.setuser(fajax.data);
-        if(check===false){
+      else{
+        if( fajax.data instanceof user){//user sign up
+          let check= database.setuser(fajax.data);
+          if(check===false){
+            statusnum=403;
+            statustext="forbidden";
+          }
+          else{
+            statusnum=200;
+            statustext="ok";
+          }
+        }
+        else{//data wasn't valid
           statusnum=403;
           statustext="forbidden";
         }
+        
+      }
+     
+
+    }
+    if(method==="GET"){
+      if(fajax.data instanceof Number){//get one
+        text= database.get(fajax.username, fajax.data);
+        if(text!=null){
+          statusnum=200;
+          statustext="ok";
+
+        }
+      
+      }
+      else{
+        if(fajax.data === ""){//get all
+          text= database.getall(fajax.username)
+          if(text!=null){
+            statusnum=200;
+            statustext="ok";
+          }
+        }
         else{
+          if(fajax.data instanceof user){//sign in
+            text= database.getuser(fajax.data);
+              if(text===null){
+                statusnum=404;
+                statustext="not found";
+              }
+              else{
+                statusnum=200;
+                statustext="ok";
+              }
+            }
+            else{//data wasn't valid
+              statusnum=403;
+              statustext="forbidden";
+          }
+        }
+        
+      }
+      
+
+      //add: getby     
+    }
+    if(method==="PUT"){//or patch? 
+      if(fajax.data instanceof contact){
+       let check=database.update(fajax.username, fajax.data);
+       if(check===true){
+        statusnum=200;
+        statustext="ok";
+       }
+      }
+      else{//data wasn't valid
+        statusnum=403;
+        statustext="forbidden";
+      }
+    }
+    if(method==="DELETE"){//or patch?
+      if(fajax.data instanceof contact){
+        let check=database.remove(fajax.user, fajax.data);
+        if(check===true){
           statusnum=200;
           statustext="ok";
         }
@@ -32,42 +104,6 @@ function serverrecieve(fajax)
       else{//data wasn't valid
         statusnum=403;
         statustext="forbidden";
-      }
-      
-
-    }
-    if(method==="GET"){
-      if(typeof data === 'number'){//get one
-        text= database.get(request.data);
-        if(text!=null){
-          statusnum=200;
-          statustext="ok";
-
-        }
-      }
-      if(typeof data === user){//get all
-        text= database.getall(request.data)
-        if(text!=null){
-          statusnum=200;
-          statustext="ok";
-        }
-      }
-      //add:
-      //getby
-      //sign in
-    }
-    if(method==="PUT"){//or patch?
-       let check=database.update(request.username, request.data);
-       if(check===true){
-        statusnum=200;
-        statustext="ok";
-      }
-    }
-    if(method==="DELETE"){//or patch?
-      let check=database.remove(request.user, request.data);
-      if(check===true){
-        statusnum=200;
-        statustext="ok";
       }
    }
    
@@ -82,10 +118,6 @@ function serverrecieve(fajax)
   /*
 things to do:
 
-fix the elses so they will be on both ifs
-make sure typeof with a type i created works
-add all the rest of the 403 elses
-sign in
 getby
   
   */  
